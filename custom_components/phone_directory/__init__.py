3dfc5ead706528ef2bd3f434dc37579d9e59482e
JeffCrum1 <94788212+JeffCrum1@users.noreply.github.com>
2026-07-10 13:24:04 -0500

    Update __init__.py

--- custom_components/phone_directory/__init__.py
+++ custom_components/phone_directory/__init__.py
@@ -16,6 +16,6 @@ async def async_setup_entry(
 ) -> bool:
     """Set up Phone Directory from a config entry."""
 
-    LOGGER.warning("Phone Directory setup complete")
+    LOGGER.info("Phone Directory setup complete")
 
     return True
