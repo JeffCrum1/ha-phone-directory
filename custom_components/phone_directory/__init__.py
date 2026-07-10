87933d45e9bead5163a44f654a71b8e4f8430d94
JeffCrum1 <94788212+JeffCrum1@users.noreply.github.com>
2026-07-10 12:39:02 -0500

    Add initial integration setup

--- custom_components/phone_directory/__init__.py
+++ custom_components/phone_directory/__init__.py
@@ -2,6 +2,16 @@
 
 import logging
 
+from homeassistant.core import HomeAssistant
+
 LOGGER = logging.getLogger(__name__)
 
-LOGGER.info("Phone Directory integration loaded")
+DOMAIN = "phone_directory"
+
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the Phone Directory integration."""
+
+    LOGGER.info("Phone Directory setup complete")
+
+    return True
